27c748fbd6c7541bbaaca071c6942c9de07fc14a
AliElsebaie <ali_elsebaie98@yahoo.com>
2019-07-01 13:32:19 +0200

    1

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 var express = require('express');
 var pg = require('pg')
 var connectionString = "postgres://postgres:Besmellah@localhost:1234/Royal_Manta";
-var app = express.app();
+var app = express();
+const {Pool} = require("pg");
+
+const pool = new Pool({
+    connectionString: connectionString,
+  })
 
 app.get('/', function (req, res, next) {    // getting all agencies 
-    pg.connect(connectionString,function(err,client,done) {
+    
+    //var pool = new pg.Pool()
+    pool.connect(function(err,client,done) {
+        console.log("mafeesh")
        if(err){
+        console.log("tele3 fee")
+
            console.log("not able to get connection "+ err);
            res.status(400).send(err);
        } 
-       client.query('SELECT * FROM Agency', [1],function(err,result) {
+       
+       pool.query('SELECT * FROM Agency', function(err,result) {
            done();    // closing the connection;
            if(err){
                console.log(err);
                res.status(400).send(err);
            }
-           res.status(200).send(result.rows);
+           res.status(200).send(result);
        });
     });
 });
