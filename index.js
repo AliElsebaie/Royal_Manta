585ed48dc5e75ee87023e48f67aef37d02536ece
AliElsebaie <ali_elsebaie98@yahoo.com>
2019-07-02 11:53:42 +0200

    m

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const pool = new Pool({
   })
 
 
-app.post('/createAgency/:name', function (req, res, next) {    // creating agencies 
+app.post('/createAgency/:name', function (req, res, next) {    // creating agency
 
     pool.connect(function(err,client,done) {
        if(err){
@@ -54,7 +54,7 @@ app.get('/getAllAgencies', function (req, res, next) {    // getting all agencie
     });
 });
 
-app.delete('/deleteAgency/:name', function (req, res, next) {    // deleting agencies 
+app.delete('/deleteAgency/:name', function (req, res, next) {    // deleting agency 
 
     pool.connect(function(err,client,done) {
        if(err){
@@ -75,22 +75,22 @@ app.delete('/deleteAgency/:name', function (req, res, next) {    // deleting age
     });
 });
 
-app.update('/updateAgency/:name', function (req, res, next) {    // deleting agencies 
+app.put('/updateAgency/:name', function (req, res, next) {    // updating agency
 
     pool.connect(function(err,client,done) {
        if(err){
            console.log("not able to get connection "+ err);
            res.status(400).send(err);
        } 
-      
-       pool.query("delete from agency where name = ($1)" ,[req.params.name]
+       console.log(req.body);
+       
+       pool.query("update agency set name = ($1) where name = ($2)" ,([req.body,req.params.name])
        , function(err,result) {
            done();    // closing the connection;
            if(err){
                console.log(err);
                res.status(400).send(err);
            }
-           console.log('deleted Successfully')
            res.status(200).send(result);
        });
     });
