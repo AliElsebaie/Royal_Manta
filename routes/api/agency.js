3d4789c1b6032c6d12937bc465b0fbec5d9e9605
madoooaboelazaiem <Madooo1998@hotmail.com>
2019-07-02 12:01:51 +0200

    mado

--- routes/api/agency.js
+++ routes/api/agency.js
@@ -6,6 +6,27 @@ var connectionString = "postgres://postgres:123456@localhost:5432/Royal";
 const pool = new Pool({
     connectionString: connectionString,
   })
+  app.post('/createAgency/:name', function (req, res, next) {    // creating agency
+
+    pool.connect(function(err,client,done) {
+       if(err){
+           console.log("not able to get connection "+ err);
+           res.status(400).send(err);
+       } 
+      
+        pool.query("insert into agency (name) values ($1)", [req.params.name]
+        , function(err,result) {
+            done();    // closing the connection;
+            if(err){
+                console.log(err);
+                res.status(400).send(err);
+            }
+            res.status(200).send(result);
+        });
+    
+ 
+    });
+});
 
 router.get('/', function (req, res, next) {    // getting all ageencies 
     
@@ -16,7 +37,48 @@ router.get('/', function (req, res, next) {    // getting all ageencies
            res.status(400).send(err);
        } 
        
-       pool.query('SELECT * FROM Agency', function(err,result) {
+       pool.query('SELECT * FROM agency.agency ORDER BY id ASC', function(err,result) {
+           done();    // closing the connection;
+           if(err){
+               console.log(err);
+               res.status(400).send(err);
+           }
+           res.status(200).send(result);
+       });
+    });
+});
+app.delete('/deleteAgency/:name', function (req, res, next) {    // deleting agency 
+
+    pool.connect(function(err,client,done) {
+       if(err){
+           console.log("not able to get connection "+ err);
+           res.status(400).send(err);
+       } 
+      
+       pool.query("delete from agency where name = ($1)" ,[req.params.name]
+       , function(err,result) {
+           done();    // closing the connection;
+           if(err){
+               console.log(err);
+               res.status(400).send(err);
+           }
+           console.log('deleted Successfully')
+           res.status(200).send(result);
+       });
+    });
+});
+
+app.put('/updateAgency/:name', function (req, res, next) {    // updating agency
+
+    pool.connect(function(err,client,done) {
+       if(err){
+           console.log("not able to get connection "+ err);
+           res.status(400).send(err);
+       } 
+       console.log(req.body);
+       
+       pool.query("update agency set name = ($1) where name = ($2)" ,([req.body,req.params.name])
+       , function(err,result) {
            done();    // closing the connection;
            if(err){
                console.log(err);
